c801b48c0dea14b6e5d47d843476785e700a6650
Krzysztof Ploch <kris@ploch.dev>
2025-05-21 13:34:50 +0200

    Tests for McMaster framework

--- tests/CommandLine.IntegrationTests/TestCommand.cs
+++ tests/CommandLine.IntegrationTests/TestCommand.cs
@@ -5,7 +5,8 @@ namespace Ploch.Common.CommandLine.Tests;
 [Command(Name = "testCommand")]
 public class TestCommand(TestCallback testCallback) : IAsyncCommand
 {
-    [Option] public string? TestArg { get; set; }
+    [Option]
+    public string? TestArg { get; set; }
 
     public Task OnExecuteAsync(CancellationToken cancellationToken = default)
     {
